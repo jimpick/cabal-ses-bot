8ce014b2034dc9db72a61fffd6609461f3fc84b9
Jim Pick <jim@backblend.com>
2018-08-11 19:04:39 -0700

    Get basic bot working

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import SES from 'ses'
 import Cabal from 'cabal-node'
 import cabalSwarm from 'cabal-node/swarm'
 import ram from 'random-access-memory'
+import sesBot, { send } from './ses-bot'
 
 const key = '062cdf507da82626175a0b8db0a6b235b9c2134b96d2376fc8ef59a9597f32a1'
 
@@ -39,44 +40,12 @@ function joinChannel (channel) {
 
 function onMessage (message) {
   const {channel, author, time, content} = message
-  console.log(`${channel} ${time} - ${author}: ${content}`)
+  // console.log(`${channel} ${time} - ${author}: ${content}`)
+  send(message)
 }
 
-function bot () {
-  const r = SES.makeSESRootRealm()
-
-  function buildCounterSrc () {
-    let def;
-
-    function Counter () {
-      let count = 0;
-      return def({
-        incr: () => { return ++count; },
-        decr: () => { return --count; },
-        get: () => count
-      });
-    }
-
-    return `${Counter}; Counter()`;
-  }
-
-  const counter = r.evaluate(buildCounterSrc())
-
-  counter.incr()
-  counter.incr()
-  counter.incr()
-  console.log(counter.get())
-  // console.log(counter.dump())
-
-  r.evaluate(`
-    incr();
-    incr();
-    log(get());
-  `, {incr: counter.incr, get: counter.get, log: console.log})
-
-  r.evaluate(`
-    decr();
-    decr();
-    log(get());
-  `, {decr: counter.decr, get: counter.get, log: console.log})
-}
+sesBot.on('message', data => {
+  // console.log('SesBot message', data)
+  const {channel, message, options} = data 
+  cabal.message(channel, message, options)
+})
---
+++ ses-bot.js
@@ -0,0 +1,119 @@
+import SES from 'ses'
+import debug from 'debug'
+import EventEmitter from 'events'
+
+class SesBot extends EventEmitter {}
+
+const sesBot = new SesBot()
+
+const debugLog = debug('ses-bot')
+
+const r = SES.makeSESRootRealm()
+
+debugLog('Debug log on')
+
+function buildBotKernelSrc () {
+  let def, log
+
+  function kernel () {
+    let pidCounter = 0
+    let messageCounter = 0
+    let handlers = []
+    let state = []
+    return def({
+      register: (botName, handlerFunc) => {
+        const pid = ++pidCounter
+        handlers[pid] = {
+          botName,
+          handlerFunc
+        }
+        log('Registered handler at PID:', pid, botName, handlerFunc)
+        return pid
+      },
+      send: message => {
+        const id = 'm' + ++messageCounter
+        log(id, `Message:`, message)
+        const results = handlers.map((handler, pid) => {
+          const {botName, handlerFunc, killed} = handler
+          const {author} = message
+          if (killed) return
+          if (author === botName) return
+          function handlerLog () {
+            log(`${id} PID ${pid}`, ...arguments)
+          }
+          try {
+            handlerLog('State:', state[pid])
+            const result = SES.confine(
+              `(${handlerFunc})(botName, message, state)`,
+              {
+                botName,
+                message,
+                state: state[pid],
+                setState: newState => state[pid] = newState,
+                emit: message => {
+                  handlerLog('Emit:', message)
+                  emit(message)
+                },
+                log: (...rest) => handlerLog('Log:', ...rest)
+              }
+            )
+            handlerLog('Success:', result)
+            return {result}
+          } catch (e) {
+            const err = {
+              name: e.name,
+              message: e.message,
+              code: e.code,
+              stack: e.stack
+            }
+            handlerLog('Fail:', err)
+            return {error: err}
+          }
+        })
+        log(id, 'Handlers finished')
+        return results
+      },
+      getLastPid: () => pidCounter,
+      ps: () => handlers,
+      kill: id => handlers[id].killed = true
+    })
+  }
+
+  return `${kernel}; kernel()`
+}
+
+const botKernel = r.evaluate(buildBotKernelSrc(), {
+  log: debugLog,
+  emit
+})
+
+function emit (message) {
+  // console.log('Emit:', message)
+  sesBot.emit('message', message)
+}
+
+function bot1 (botName, message, state) {
+  if (!state) state = {counter: 0}
+  let {counter} = state
+  const {channel, content, author} = message
+  log(message)
+  const regex = new RegExp(`^${botName}[ :] *(.*)$`)
+  const match = content.match(regex)
+  if (!match) return counter
+  const rest = match[1]
+  counter++
+  emit({
+    channel,
+    message: `${author}: Echo "${rest}"`
+  })
+  setState({counter})
+  return counter
+}
+botKernel.register('ses-bot', bot1)
+
+export function send (message) {
+  botKernel.send(message)
+}
+
+export default sesBot
+
