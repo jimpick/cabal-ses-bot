7ac8575f206740ca42e9e236468f5dc7a7778a70
Jim Pick <jim@backblend.com>
2018-08-15 19:40:00 -0700

    Change logging examples

--- sample-bots/counter-bot.js
+++ sample-bots/counter-bot.js
@@ -4,11 +4,10 @@ async function handleMessage (botName, message, state, refs) {
   if (!state) state = {counter: 0}
   let {counter} = state
   const {channel, content, author} = message
-  console.log(message)
   const regex = new RegExp(`^${botName}([ :] *(.*))?$`)
   const match = content.match(regex)
-  console.log('match', match)
   if (!match) return
+  console.log(`Message: (${channel}) ${author}: "${content}"`)
   const command = match[2] ? match[2].trim().toLowerCase() : null
   if (command === 'incr') {
     counter++
--- sample-bots/echo-bot.js
+++ sample-bots/echo-bot.js
@@ -2,10 +2,10 @@ module.exports = handleMessage
 
 async function handleMessage (botName, message, state, refs) {
   const {channel, content, author} = message
-  console.log(message)
   const regex = new RegExp(`^${botName}[ :] *(.*)$`)
   const match = content.match(regex)
   if (!match) return
+  console.log(`Message: (${channel}) ${author}: "${content}"`)
   const rest = match[1]
   chat.send({
     channel,
